77994c51bc5c9e47c2ba97cddf7d8937594eb010
Kevin <ukd210@gmail.com>
2026-04-18 22:22:32 +0900

    modified:   tests/dags/user_processing.py

---
+++ tests/dags - origin/user_processing.py
@@ -0,0 +1,65 @@
+# from airflow.sdk import dag
+# from airflow.models import DAG
+from airflow.decorators import dag, task
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.decorators import task
+from datetime import datetime
+from airflow.sensors.base import BaseSensorOperator, PokeReturnValue
+import requests
+# import pandas as pd
+
+@dag(
+    dag_id="user_processing",
+    start_date=datetime(2026, 4, 19),
+    schedule="@daily",
+    catchup=False,
+    tags=["data-processing"],
+)
+
+def user_processing():
+    
+    @task
+    def extract_user_data():
+        # 테이블 생성 task
+        create_table = SQLExecuteQueryOperator(
+            task_id='create_table',
+            sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                id INT PRIMARY KEY,
+                firstname VARCHAR(255),
+                lastname VARCHAR(255),
+                email VARCHAR(255),
+                created_at timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            """,
+            conn_id='postgres',
+            database='airflow'
+        )
+        create_table()
+
+    # 데이터 변환 task
+    @task.sensor(poke_interval=30, timeout=300)
+    def is_api_available()  -> PokeReturnValue:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
+        response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json", headers=headers)
+        print(f"API Response Status Code: {response.status_code}")
+        if  response.status_code == 200:
+            condition = True
+            data = response.json()
+            ip_address = data['metadata']['ipAddress']
+        else:
+            condition = False
+            ip_address = None
+        
+        return PokeReturnValue(is_done=condition, xcom_value={'ipAddress': ip_address})
+
+    # Task 의존성 설정
+    extract_user_data() >> is_api_available()
+
+class CheckUserDataSensor(BaseSensorOperator):
+    def poke(self, context):
+        # 조건 체크: 예를 들어, users 테이블에 데이터가 있는지 확인
+        # 간단한 예시로 True를 반환
+        return PokeReturnValue(is_done=True, xcom_value={'status': 'data_ready', 'message': 'User data is available'})
+
+user_processing()
---
+++ tests/dags/user_processing2.py
@@ -0,0 +1,51 @@
+# from airflow.sdk import dag
+# from airflow.models import DAG
+from airflow.decorators import dag, task
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.decorators import task
+from datetime import datetime
+# import pandas as pd
+
+@dag(
+    dag_id="user_processing2",
+    start_date=datetime(2026, 4, 19),
+    schedule="@daily",
+    catchup=False,
+    tags=["data-processing"],
+)
+
+def user_processing2():
+    
+    @task
+    def extract_user_data():
+        # 테이블 생성 task
+        create_table = SQLExecuteQueryOperator(
+            task_id='create_table',
+            sql="""
+            CREATE TABLE IF NOT EXISTS users (
+                id INT PRIMARY KEY,
+                firstname VARCHAR(255),
+                lastname VARCHAR(255),
+                email VARCHAR(255),
+                created_at timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            """,
+            conn_id='postgres',
+            database='airflow'
+        )
+        create_table()
+
+    # 데이터 변환 task
+    # @task
+    # def transform_user_data(sql_result):
+    #     """SQL 쿼리 결과를 변환합니다"""
+    #     df = pd.DataFrame(sql_result)
+    #     # 데이터 변환 로직 추가
+    #     df['processed_date'] = datetime.now()
+    #     return df.to_dict('records')
+
+    # # Task 의존성 설정
+    # result = create_table
+    # transformed_data = transform_user_data(result)
+
+user_processing2()
